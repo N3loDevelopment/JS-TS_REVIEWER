3d2d585100c269f5b7cde576526ae89ad68ed592
N3loDevelopment <NeloDevelopment@gmx.de>
2025-07-09 18:02:53 +0200

    changed to a test file

--- src/FileHandler.ts
+++ src/FileHandler.ts
@@ -55,16 +55,4 @@ class FileHandler {
     }
 }
 
-
-function test() {
-    let old = 0;
-    let lol = true
-    let dfhbgdushg = 0;
-    if (lol)
-        log(eval("2+2"));
-    if (old == 7) {
-        log("test2");
-    }
-}
-
 export const fileHandler = new FileHandler();
---
+++ src/TestFile.js
@@ -0,0 +1,10 @@
+function test() {
+    let old = 0;
+    let lol = true
+    let dfhbgdushg = 0;
+    if (lol)
+        log(eval("2+2"));
+    if (old == 7) {
+        log("test2");
+    }
+}
